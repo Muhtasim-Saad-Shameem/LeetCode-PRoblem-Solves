c203265646a0fc50091b5f855f9895dc16943359
Muhtasim Saad Shameem <msahil0023@gmail.com>
2026-01-04 23:55:14 +0600

    first task

---
+++ Multiply Strings.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        # Maximum length of product is len(num1) + len(num2)
+        result = [0] * (len(num1) + len(num2))
+        
+        # reversing both num (right to left)
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        
+        #  each digit of num1 x each digit of num2
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit1 = int(num1[i])
+                digit2 = int(num2[j])
+                product = digit1 * digit2
+                
+                # Add product
+                result[i + j] += product
+                
+                # we handle the carry 
+                result[i + j + 1] += result[i + j] // 10
+                result[i + j] %= 10
+        
+        while len(result) > 1 and result[-1] == 0:
+            result.pop()
+        
+        result.reverse()
+            
+        return ''.join(map(str, result))
